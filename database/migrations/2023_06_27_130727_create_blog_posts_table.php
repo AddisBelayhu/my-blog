e92f412445c2c10fecce697b763d73cbc6f5962e
AddisBelayhu <addisgithub@gmail.com>
2023-06-29 10:55:31 +0300

    add controller

--- database/migrations/2023_06_27_130727_create_blog_posts_table.php
+++ database/migrations/2023_06_27_130727_create_blog_posts_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateBlogPostsTable extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->id();
+            $table->text('title');
+            $table->text('body');
+            $table->text('user_id');
             $table->timestamps();
         });
     }
@@ -20,7 +23,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('blog_posts');
     }
